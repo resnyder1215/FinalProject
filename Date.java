c3fd8e5fe9ffeedafacbaf18051fd853bcfeea34
WARlord1903 <31146829+WARlord1903@users.noreply.github.com>
2021-11-15 15:22:18 -0500

    Added Javadoc comments
    Added Javadoc comments for Time and Date implementation

--- Date.java
+++ Date.java
@@ -5,56 +5,105 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/**
+ *
+ * @author willi
+ */
 public class Date {
     private int day;
     private int month;
     private int year;
     String dateStr;
     
+    /**
+     * Default Constructor
+     */
     public Date(){
     }
 
+    /**
+     *
+     * @param day the day that is entered for the date
+     * @param month the month that is entered for the date
+     * @param year the year that is entered for the date
+     */
     public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
+        //Formats date into string
         this.dateStr = String.format("%02d/%02d/%04d", this.day, this.month, this.year);
     }
 
+    /**
+     *
+     * @return the day contained in the Date object
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     *
+     * @return the month contained in the Date object
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     *
+     * @return the year contained in the Date object
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     *
+     * @return a formatted string that shows the date held by the object
+     */
     public String toString() {
+        //Formats date into string
         this.dateStr = String.format("%02d/%02d/%04d", this.month, this.day, this.year);
         return dateStr;
     }
 
+    /**
+     *
+     * @param day the day to set the Date object to
+     */
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     *
+     * @param month the month to set the Date object to
+     */
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     *
+     * @param year the year to set the Date object to
+     */
     public void setYear(int year) {
         this.year = year;
     }
     
+    /**
+     *
+     * @param input the Scanner object used to read the input
+     * @return a new Date object
+     */
     public static Date inputDate(Scanner input){
         int month;
         int day;
         int year;
         String date;
+        //regex, looks for String in "MM/DD/YYYY" pattern
         Pattern p = Pattern.compile("([0-9]{1,2})/([0-9]{2})/([0-9]{1,4})");
         while(true){
             date = input.nextLine();
@@ -66,12 +115,14 @@ public class Date {
                 day = Integer.parseInt(m.group(2));
                 year = Integer.parseInt(m.group(3));
                 switch(month){
+                    //months with 30 days
                     case 4: case 6: case 9: case 11:
                         if(day < 1 || day > 30){
                             System.out.println("ERROR! Invalid date entered! Please enter a valid date.");
                             continue;
                         }
                         break;
+                    //months with 31 days
                     case 1: case 3: case 5: case 7:
                     case 8: case 10: case 12:
                         if(day < 1 || day > 31){
@@ -79,7 +130,9 @@ public class Date {
                             continue;
                         }
                         break;
+                    //February
                     case 2:
+                        //accounts for leap years
                         if(day < 1 || (year % 4 != 0 && day > 28) || (year % 4 == 0 && day > 29)){
                             System.out.println("ERROR! Invalid date entered! Please enter a valid date.");
                             continue;
@@ -89,11 +142,12 @@ public class Date {
                         System.out.println("ERROR! Invalid date entered! Please enter a valid date.");
                         continue;
                 }
+                //4-digit years only
                 if(year < 1 || year > 9999){
                     System.out.println("ERRROR! Invalid date entered! Please enter a valid date.");
                     continue;
                 }
-               
+                
                 return new Date(day, month, year);
             }
         }
--- Time.java
+++ Time.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-
+/**
+ *
+ * @author willi
+ */
 public class Time {
     private int hours;
     private int minutes;
     private int seconds;
     String timeStr;
     
+    /**
+     * Default constructor
+     */
     public Time() {
     }
 
+    /**
+     *
+     * @param hours the hour that is entered for the time
+     * @param minutes the minute that is entered for the time
+     * @param seconds the second that is entered for the time
+     */
     public Time(int hours, int minutes, int seconds) {
         this.hours = hours;
         this.minutes = minutes;
@@ -21,40 +33,74 @@ public class Time {
         this.timeStr = String.format("%02d:%02d:%02d", this.hours, this.minutes, this.seconds);
     }
 
+    /**
+     *
+     * @return the hour contained in the Time object
+     */
     public int getHours() {
         return hours;
     }
 
+    /**
+     *
+     * @return the minute contained in the Time object
+     */
     public int getMinutes() {
         return minutes;
     }
 
+    /**
+     *
+     * @return the second contained in the Time object
+     */
     public int getSeconds() {
         return seconds;
     }
     
+    /**
+     *
+     * @return a formatted string that shows the time held by the object
+     */
     public String toString(){
         this.timeStr = String.format("%02d:%02d:%02d", this.hours, this.minutes, this.seconds);
         return timeStr;
     }
 
+    /**
+     *
+     * @param hours the hour to set the Time object to
+     */
     public void setHours(int hours) {
         this.hours = hours;
     }
 
+    /**
+     *
+     * @param minutes the minute to set the Time object to
+     */
     public void setMinutes(int minutes) {
         this.minutes = minutes;
     }
 
+    /**
+     *
+     * @param seconds the second to set the Time object to
+     */
     public void setSeconds(int seconds) {
         this.seconds = seconds;
     }
     
+    /**
+     *
+     * @param input the Scanner object used to read the input
+     * @return a new Time object
+     */
     public static Time inputTime(Scanner input){
         int hours = 0;
         int minutes = 0;
         int seconds = 0;
         String time;
+        //regex, looks for String in "HH:MM:SS" pattern
         Pattern p = Pattern.compile("([0-9]{1,2}):([0-9]{2})(:[0-9]{2})?");
         while(true){
             time = input.nextLine();
@@ -64,8 +110,10 @@ public class Time {
             else{
                 hours = Integer.parseInt(m.group(1));
                 minutes = Integer.parseInt(m.group(2));
+                //if seconds were provided (optional)
                 if(m.groupCount() > 3)
                     seconds = Integer.parseInt(m.group(3));
+                //validates that the time is valid
                 if(hours < 0 || hours > 24 || minutes < 0 || minutes > 60 || seconds < 0 || seconds > 60){
                     System.out.println("ERROR! Invalid time format. Please type a real time.");
                     continue;
