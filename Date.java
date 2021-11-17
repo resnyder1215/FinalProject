16f95e2ce52b0cfc19467a5ba541da6c9a2ec6b3
WARlord1903 <31146829+WARlord1903@users.noreply.github.com>
2021-11-17 15:52:00 -0500

    Added 1-digit input option for Day

--- Date.java
+++ Date.java
@@ -104,7 +104,7 @@ public class Date {
         int year;
         String date;
         //regex, looks for String in "MM/DD/YYYY" pattern
-        Pattern p = Pattern.compile("([0-9]{1,2})/([0-9]{2})/([0-9]{1,4})");
+        Pattern p = Pattern.compile("([0-9]{1,2})/([0-9]{1,2})/([0-9]{1,4})");
         while(true){
             date = input.nextLine();
             Matcher m = p.matcher(date);
